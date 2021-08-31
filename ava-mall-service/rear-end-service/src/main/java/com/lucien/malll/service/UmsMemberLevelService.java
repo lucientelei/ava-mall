0c2f8765ea99d964693a79a3cea6c7ca3608d393
CHENXDDD <q243341364@1216.com>
2021-08-31 09:05:00 +0800

    [UPDATE]memberlevel

---
+++ ava-mall-service/rear-end-service/src/main/java/com/lucien/malll/service/UmsMemberLevelService.java
@@ -0,0 +1,20 @@
+package com.lucien.malll.service;
+
+import com.lucien.mall.pojo.UmsMemberLevel;
+
+import java.util.List;
+
+/**
+ * @Author Lucien
+ * @Date 2021/8/31
+ * 会员等级管理Service
+ */
+public interface UmsMemberLevelService {
+
+    /**
+     * 获取所有用户等级
+     * @return
+     */
+    List<UmsMemberLevel> list(Integer defaultStatus);
+
+}
---
+++ ava-mall-service/rear-end-service/src/main/java/com/lucien/malll/service/impl/UmsMemberLevelServiceImpl.java
@@ -0,0 +1,33 @@
+package com.lucien.malll.service.impl;
+
+import com.lucien.mall.mapper.UmsMemberLevelMapper;
+import com.lucien.mall.pojo.UmsMemberLevel;
+import com.lucien.mall.pojo.UmsMemberLevelExample;
+import com.lucien.malll.service.UmsMemberLevelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author Lucien
+ * @Date 2021/8/31
+ * 会员等级管理Impl
+ */
+@Service
+public class UmsMemberLevelServiceImpl implements UmsMemberLevelService {
+
+    @Autowired
+    private UmsMemberLevelMapper memberLevelMapper;
+
+    /**
+     * 获取所有用户等级
+     * @return
+     */
+    @Override
+    public List<UmsMemberLevel> list(Integer defaultStatus) {
+        UmsMemberLevelExample example = new UmsMemberLevelExample();
+        example.createCriteria().andDefaultStatusEqualTo(defaultStatus);
+        return memberLevelMapper.selectByExample(example);
+    }
+}
